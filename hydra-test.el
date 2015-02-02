5d576aed8ee0f1ee29c2d1ca597301bdcff5597e
Oleh Krehel <ohwoeowho@gmail.com>
2015-02-02 15:39:51 +0100

    hydra-test.el: Add licence

--- hydra-test.el
+++ hydra-test.el
@@ -1,3 +1,29 @@
+;;; hydra-test.el --- Tests for Hydra
+
+;; Copyright (C) 2015  Free Software Foundation, Inc.
+
+;; Author: Oleh Krehel
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+
 (require 'ert)
 
 (ert-deftest defhydra-red-error ()
@@ -200,3 +226,5 @@ The body can be accessed via `toggle/body'."
                t)))))))
 
 (provide 'hydra-test)
+
+;;; hydra-test.el ends here
