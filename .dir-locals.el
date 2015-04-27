dd02b166ee8e566f3826fb871816352395a8cd20
Oleh Krehel <ohwoeowho@gmail.com>
2015-04-27 14:31:50 +0200

    .dir-locals.el: Add

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode
+  (bug-reference-url-format . "https://github.com/abo-abo/hydra/issues/%s")
+  (indent-tabs-mode . nil)))
