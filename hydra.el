e0454a100541ce3f1f732b97894a3441cef3316f
Oleh Krehel <ohwoeowho@gmail.com>
2016-03-04 20:57:06 +0100

    Add .elpaignore
    Fixes #182

--- hydra.el
+++ hydra.el
@@ -5,7 +5,7 @@
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; Maintainer: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/hydra
-;; Version: 0.13.4
+;; Version: 0.13.5
 ;; Keywords: bindings
 ;; Package-Requires: ((cl-lib "0.5"))
 
