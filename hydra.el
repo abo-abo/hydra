943636fe4a35298d9d234222bc4520dec9ef2305
Oleh Krehel <ohwoeowho@gmail.com>
2017-07-22 17:18:08 +0200

    hydra.el: Bump version

--- hydra.el
+++ hydra.el
@@ -5,7 +5,7 @@
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; Maintainer: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/hydra
-;; Version: 0.13.6
+;; Version: 0.14.0
 ;; Keywords: bindings
 ;; Package-Requires: ((cl-lib "0.5"))
 
