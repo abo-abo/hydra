d071278fcc0a62d10c3a3d8a1adf33461a90a0aa
Oleh Krehel <ohwoeowho@gmail.com>
2018-10-30 20:25:43 +0100

    hydra.el (hydra--format): Fix off-by-one
    * hydra.el (hydra--hint): Don't add an extra newline.
    
    * hydra-test.el (hydra-column-order): One newline less in expected string.
    Also, re-indent the test code.
    
    Fixes #303

--- hydra.el
+++ hydra.el
@@ -559,7 +559,7 @@ BODY, and HEADS are parameters to `defhydra'."
          (heads-w-col (cl-remove-if-not (lambda (heads) (hydra--head-property (nth 0 heads) :column)) sorted-heads))
          (heads-wo-col (cl-remove-if (lambda (heads) (hydra--head-property (nth 0 heads) :column)) sorted-heads)))
     (concat (when heads-w-col
-              (concat "\n" (hydra--hint-from-matrix body (hydra--generate-matrix heads-w-col))))
+              (hydra--hint-from-matrix body (hydra--generate-matrix heads-w-col)))
             (when heads-wo-col
               (hydra--hint-heads-wocol body (car heads-wo-col))))))
 
@@ -717,7 +717,7 @@ The expressions can be auto-expanded according to NAME."
                         (substring docstring (+ start offset 1 lspec varp))))))))
       (cond
         ((string= docstring "")
-         (substring rest 1))
+         rest)
         ((eq ?\n (aref docstring 0))
          `(concat (format ,(substring docstring 1) ,@(nreverse varlist))
                   ,rest))
